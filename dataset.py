c9b7bc314d54ebfa601547e4519e816456dd144c
ndrplz <ndrplz@gmail.com>
2017-12-07 15:58:14 +0100

    Implement parsing of *-info.mat files
    * Added `_parse_NORB_info_file` to parse `*-info.mat` files
    * Header parsing is now encapsulated in `_parse_small_NORB_header`

--- dataset.py
+++ dataset.py
@@ -29,26 +29,39 @@ class SmallNORBDataset:
                       for key in ['cat', 'info', 'dat']}
         self.dataset_files = {'train': train_files, 'test': test_files}
 
-        self.train_images = self._parse_NORB_data_file(self.dataset_files['train']['dat'])
-        self.test_images  = self._parse_NORB_data_file(self.dataset_files['test']['dat'])
+        self.train_dat = self._parse_NORB_data_file(self.dataset_files['train']['dat'])
+        self.test_dat  = self._parse_NORB_data_file(self.dataset_files['test']['dat'])
+
+        self.train_info = self._parse_NORB_info_file(self.dataset_files['train']['info'])
+        self.test_info  = self._parse_NORB_info_file(self.dataset_files['test']['info'])
+
+    @staticmethod
+    def _parse_small_NORB_header(file_pointer):
+
+        # Read magic number
+        magic = struct.unpack('<BBBB', file_pointer.read(4))  # '<' is little endian)
+        print('Magic number: {} --> {}'.format(magic, matrix_type_from_magic(magic)))
+
+        # Read dimensions
+        dimensions = []
+        num_dims, = struct.unpack('<i', file_pointer.read(4))  # '<' is little endian)
+        for _ in range(num_dims):
+            dimensions.extend(struct.unpack('<i', file_pointer.read(4)))
+        print('Dimensions: {}'.format(dimensions))
+
+        file_header_data = {'magic_number': magic,
+                            'matrix_type': matrix_type_from_magic(magic),
+                            'dimensions': dimensions}
+        return file_header_data
 
     @staticmethod
     def _parse_NORB_data_file(file_path):
 
         with open(file_path, mode='rb') as f:
 
-            # Read magic number
-            magic = struct.unpack('<BBBB', f.read(4))  # '<' is little endian)
-            print('Magic number: {} --> {}'.format(magic, matrix_type_from_magic(magic)))
+            header = SmallNORBDataset._parse_small_NORB_header(f)
 
-            # Read dimensions
-            dimensions = []
-            num_dims, = struct.unpack('<i', f.read(4))  # '<' is little endian)
-            for _ in range(num_dims):
-                dimensions.extend(struct.unpack('<i', f.read(4)))
-            print('Dimensions: {}'.format(dimensions))
-
-            num_examples, channels, height, width = dimensions
+            num_examples, channels, height, width = header['dimensions']
 
             examples = np.zeros(shape=(num_examples * channels, height, width), dtype=np.uint8)
 
@@ -62,12 +75,32 @@ class SmallNORBDataset:
 
         return examples
 
+    @staticmethod
+    def _parse_NORB_info_file(file_path):
+
+        with open(file_path, mode='rb') as f:
+
+            header = SmallNORBDataset._parse_small_NORB_header(f)
+
+            struct.unpack('<BBBB', f.read(4))  # ignore this integer
+
+            num_examples, num_info = header['dimensions']
+
+            examples = np.zeros(shape=(num_examples, num_info), dtype=np.int32)
+
+            for r in tqdm(range(num_examples), desc='Loading info...'):
+                for c in range(num_info):
+                    info, = struct.unpack('<i', f.read(4))
+                    examples[r, c] = info
+
+        return examples
+
 
 if __name__ == '__main__':
 
     plt.ion()
 
     dataset = SmallNORBDataset(dataset_root='/media/minotauro/DATA/smallnorb/')
-    for image in dataset.test_images:
+    for image in dataset.test_dat:
         plt.imshow(image, cmap='gray')
         plt.waitforbuttonpress()
